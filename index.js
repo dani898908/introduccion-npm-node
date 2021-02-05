00110329134e1c3095c6065ff2f564ccdd8ed0b5
daniela <danielabonillac@hotmail.com>
2021-02-05 15:06:17 +0100

    fichero CRLF

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports={
+    hola: function(){
+        console.log("hola npm");
+    },
+    holaPersonalizado: function (nombre) {
+        console.log("hola" `${nombre}`+"curso npm");
+    },
+    holaCursoNPM:function () {
+        console.log("hola caracola")
+    }
+}
